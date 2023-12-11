59174ff3b3c554067bfb9b67db14258b470cfab3
eryilmazburak <144610494+eryilmazburak@users.noreply.github.com>
2023-12-11 22:56:47 +0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/inc5q3wACtcZULZL/scene.splinecode');
